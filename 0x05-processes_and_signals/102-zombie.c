8e40f4593630f39c86bd73a74ad5203758f3523a
Camaltra <mickael.boillaud@gmail.com>
2022-02-18 14:17:27 +0000

    Adding zombies

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -32,7 +32,7 @@ int main(void)
 		zombies = fork();
 		if (zombies == 0)
 			return (0);
-		printf("Zombie process created, PID: %d\n", zombies);
+		printf("Zombie process created, PID: %d", zombies);
 	}
 
 	infinite_while();
