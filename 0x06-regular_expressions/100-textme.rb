d72170dd1666d1b142c59147cee0bdaf7e6954b4
Camaltra <mickael.boillaud@gmail.com>
2022-02-23 17:42:06 +0000

    Delete the commas at the end of the file

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/(?<=from:|to:|flags:)(.*?\])/).join.sub("]",",").sub("]",",").sub("]",",")
+puts ARGV[0].scan(/(?<=from:|to:|flags:)(.*?\])/).join.sub("]",",").sub("]",",").sub("]","")
