8893862a285bd72677af2c51f40f91b4f9a542e6
Camaltra <mickael.boillaud@gmail.com>
2022-04-15 16:53:23 +0000

    Fix task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 #Install the puppet-link package
 
-exec { 'Install puppet-lint':
-    command => 'sudo gem intall puppet-lint -v 2.5.0',
+exec { 'sudo gem intall puppet-lint -v 2.5.0':
     path    => ['/usr/bin'],
 }
