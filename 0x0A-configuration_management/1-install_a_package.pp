57dd3309a9ff56d1eb15ca7032ca4fe41bf7353a
Camaltra <mickael.boillaud@gmail.com>
2022-04-15 16:48:44 +0000

    Fix task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 #Install the puppet-link package
 
-package { 'puppet-link':
-    ensure   => 2.5.0,
-    provider => gem
+package { 'puppet-lint':
+    ensure   => '2.5.0',
+    provider => 'gem'
 }
