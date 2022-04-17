cf5515df2a85ee7e0a3cb619b260718d612e9c5d
Camaltra <mickael.boillaud@gmail.com>
2022-04-17 12:09:16 +0000

    Fix 100 task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,10 @@
 #Create the good ssh config thank to puppet
 #(Need the same result as the last task)
 
-exec {'ssh ubuntu@34.148.245.248; echo "    PasswordAuthentication no\n    IdentityFile ~/.ssh/school" > /etc/ssh/ssh_config':}
+exec {sed -i "s/#   PasswordAuthentication yes/    PasswordAuthentication no/" /etc/ssh/ssh_config:
+    path => '/usr/bin',
+}
+
+exec {sed -i "s/#   IdentityFile\s~/.ssh/id_rsa/    IdentityFile ~/.ssh/school/" /etc/ssh/ssh_config:
+    path => '/usr/bin',
+}
