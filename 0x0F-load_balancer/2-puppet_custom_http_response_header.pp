607ab44396814af1a1041355d4bdce06d75bcab9
Camaltra <mickael.boillaud@gmail.com>
2022-04-25 12:33:58 +0000

    Adding advenced

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,21 +1,24 @@
 #Create Nginx configuratio server via puppet
 
+exec { 'update':
+  command => '/usr/bin/apt-get update',
+}
+
 package { 'nginx':
   ensure   => present,
-  provider => 'apt'
+  name     => 'nginx',
+  require  => Exec['update'],
 }
 
 file_line { 'Add header':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'add_header X-Served-By $hostname;',
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'add_header X-Served-By $hostname;',
   require => Package['nginx'],
 }
 
 service { 'nginx':
   ensure     => running,
-  enable     => true,
-  hasrestart => true,
   require    => Package['nginx'],
 }
